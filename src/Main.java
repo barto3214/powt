0dfa4e74395e1a92577c25105e22b80197b0c28a
barto3214 <bartek.slusarz@onet.pl>
2024-09-20 09:34:09 +0200

    zad 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        ArrayList<Integer> parzyste = new ArrayList<>();
+        ArrayList<Integer> nieparzyste = new ArrayList<>();
+        int [] wylos = new  int[100];
+        for (int i = 0; i < wylos.length; i++) {
+            wylos[i] = (int) (Math.random()*100 +1);
+
+        }
+        for (int i = 0; i < wylos.length; i++) {
+            if(i % 2 == 0){
+                parzyste.add(i);
+
+            }
+            else{
+                nieparzyste.add(i);
+            }
+        }
+        System.out.println(parzyste);
+        System.out.println(nieparzyste);
     }
 }
